3ef4fcf62f8c10dfafcbb02541f41fff751dcb24
LordMascachapas <alfredm3007@gmail.com>
2018-05-08 18:16:09 +0200

    Manage menu as interface with DB

--- reader.py
+++ reader.py
@@ -3,21 +3,26 @@ import RPi.GPIO as GPIO
 import MFRC522
 import signal
 import time
+from google_api_connector import get_users_json
+import hashlib
 
 sentinel = True
 continue_reading = True
 
 # We will use this global variables in TIMER()
-timer = 0
-time_list = [0]
+main_timer = 0
+times_between_denieds_list = [0]
 times_denied = 0
 start_timer = 0
 
-#Use those to select how many calls in how much time(in secs) you want to call BLOCK()
+#Use those to choose how many calls in how much time(in secs) you want to call BLOCK()
 max_denied = 5
 max_time = 30
 
+# Use this one to choose how much time will be the reader stopped (in secs)
+blocked_time = 30
 
+# This stops the script and clean the raspberry pins
 def end_read(signal,frame):
     global sentinel
     global continue_reading
@@ -26,56 +31,65 @@ def end_read(signal,frame):
     continue_reading = False
     GPIO.cleanup()
 
-# This update the log and freeze the loop by 30 sec
-def BLOCK():
-    time.sleep(30)
-
 # When this has been called max_denies times in max_time secs, calls BLOCK()
-def TIMER():
-    global timer
-    global time_list
+def brute_force_avoid():
+    global main_timer
+    global times_between_denieds_list
     global times_denied
     global start_timer
     global max_denied
     global max_time
+    print("NOPE")
     if times_denied > 0:
         end_timer = time.time()
-        time_list = time_list + [end_timer - start_timer]
-        timer = timer + (end_timer - start_timer)
+        times_between_denieds_list = times_between_denieds_list + [end_timer - start_timer]
+        main_timer = main_timer + (end_timer - start_timer)
     start_timer = time.time()
     times_denied += 1
+    time.sleep(1)
     if times_denied == max_denied:
-        if timer <= max_time:
-            BLOCK()
-            timer = 0
+        if main_timer <= max_time:
+            time.sleep(blocked_time)
+            main_timer = 0
             times_denied = 0
-            time_list = [0]
+            times_between_denieds_list = [0]
         else:
-            timer -= time_list[0]
-            time_list = time_list[1:]
+            main_timer -= times_between_denieds_list[0]
+            times_between_denieds_list = times_between_denieds_list[1:]
             times_denied -= 1
+
+# This calls the remote database and return a dictionary with the users data
+def get_codebook():
+    book = get_users_json()
+    return book
+
+def hasher(str):
+    return hashlib.sha256(str).hexdigest()
+
+def open_door():
+    print("OPEN ...")
+    GPIO.output(7, False)
+    time.sleep(2)
+    GPIO.output(7, True)
 
 
 signal.signal(signal.SIGINT, end_read) # This calls end_read when the program capture Ctrl+C
+codebook = get_codebook()
+print("Reader activated")
 while sentinel:
     continue_reading = True
-    abrir_puerta = False
     MIFAREReader = MFRC522.MFRC522() # We create an object which drives the hardware
     GPIO.output(7, True)
     while continue_reading:
-        if abrir_puerta: # here we choose active the GPIO 7 when the tag's card is registered
-            GPIO.output(7, False)
-            time.sleep(2)
-            GPIO.output(7, True)
-            abrir_puerta = False
-            continue_reading = False
         (detected,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL) # This scan for new cards
         (status,uid) = MIFAREReader.MFRC522_Anticoll() # This takes the uid card as a list called uid
         if detected == MIFAREReader.MI_OK:
             print("Card detected")
         if status == MIFAREReader.MI_OK:
+            hash_uid = hasher(''.join(str(e) for e in uid))
             print(uid)
-            if True:    # TODO: here we should call a database authenticator and not True
-                abrir_puerta = True # if the card is registered, we can active the GPIO 7 on the next loop travel
+            if hash_uid in codebook and codebook[hash_uid]["status"] == "AUTORIZADO":  # TODO: Call a log
+                open_door()
+                continue_reading = False
             else:
-                TIMER()
+                brute_force_avoid()
