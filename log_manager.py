71ef42741070b8a8727e007dfdcd2ff2241bcd56
Yadkee <yadkee@gmail.com>
2018-09-18 18:05:47 +0200

    Rewrote log manager

--- log_manager.py
+++ log_manager.py
@@ -1,29 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-#
-#  LOG TABLE
-# | Timestamp | uid_hashed | username | action |
-import time
-import datetime
-import json
+#! python3
+from time import strftime
+from json import load, dump
+
+# Log table
+# | Timestamp | hashedUid | Username | Action |
 
 
-def read_json_file():
-    with open('log.json') as log_json_file:
-        return json.load(log_json_file)
-
-
-def write_json_file(log_data):
-    with open('log.json', 'w') as outfile:
-        json.dump(log_data, outfile)
-
-
-# new_log_entry creates a new entry in the log.json file
-def new_log_entry(uid_hashed="xxx", username="unknown", action="not especified"):
+def add_entry(hashedUid, username, action):
     try:
-        log_data = read_json_file()
-    except:
-        log_data = {}
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%d/%m/%Y %H:%M:%S')
-    log_data[timestamp] = {"uid_hashed": uid_hashed, "username": username, "action": action}
-    write_json_file(log_data)
+        with open("log.json") as f:
+            dataLog = load(f)
+    except FileNotFoundError:
+        dataLog = {}
+    timestamp = strftime("%d/%m/%Y %H:%M:%S")
+    dataLog[timestamp] = {"hashedUid": hashedUid, "username": username,
+                          "action": action}
+    with open("log.json") as f:
+        dump(dataLog, f)
