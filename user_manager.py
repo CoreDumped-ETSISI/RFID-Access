ac65428a2e3e3f249ee8f10afba1e32279a94370
Yadkee <yadkee@gmail.com>
2018-09-18 18:05:39 +0200

    Rewrote user manager

--- user_manager.py
+++ user_manager.py
@@ -1,34 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-
-import json
-from google_api_connector import insert_user
-from google_api_connector import get_users_json
+#! python3
+from google_api import insert_user
+from google_api import get_users
+from json import load, dump
 
 
-def read_users_file():
-    with open('users.json') as log_json_file:
-        return json.load(log_json_file)
+def add_entry(hashedUid, status, name, email, phone, telegramName):
+    with open("users.json") as f:
+        userData = load(f)
+    insert_user(hashedUid, status, name, email, phone, telegramName)
+    userData[hashedUid] = {"status": status, "name": name, "email": email,
+                           "phone": phone, "telegramName": telegramName}
+    with open("users.json", "w") as f:
+        dump(userData, f)
 
 
-def write_users_file(log_data):
-    with open('users.json', 'w') as outfile:
-        json.dump(log_data, outfile)
-
-
-# new_log_entry creates a new entry in the user.json file
-def new_user_entry(uid_hashed, status="NONE", name="NONE", email="NONE", phone="NONE", telegram_name="NONE"):
-    users_data = read_users_file()
-    users_data[uid_hashed] = {"status": status, "name": name, "email": email, "phone": phone,
-                              "telegram_name": telegram_name, }
-    insert_user(uid_hashed, status, name, email, phone, telegram_name)
-    write_users_file(users_data)
-
-
-# This return the users data from local or remote database
-def get_user_dictionary():
+def get_dict():
     try:
-        users_dictionary = get_users_json()
-    except:
-        users_dictionary = read_users_file()
-    return users_dictionary
+        userData = get_users()
+    except Exception:
+        with open("users.json") as f:
+            userData = load(f)
+    return userData
