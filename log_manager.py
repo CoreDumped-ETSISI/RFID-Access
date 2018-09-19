e54e4691448c1377f9104a7403720662f866ff7c
Yadkee <yadkee@gmail.com>
2018-09-19 18:45:49 +0200

    Removed unused code and changed json format to a txt append

--- log_manager.py
+++ log_manager.py
@@ -1,24 +1,8 @@
 #! python3
-import logging
 from time import strftime
-from json import load, dump
-
-logger = logging.getLogger("Log")
-logger.setLevel(logging.DEBUG)
-
-# Log table
-# | Timestamp | hashedUid | Username | Action |
 
 
 def add_entry(hashedUid, username, action):
-    try:
-        with open("log.json") as f:
-            dataLog = load(f)
-    except FileNotFoundError:
-        logging.warn("Log was not found. Empty dict")
-        dataLog = {}
     timestamp = strftime("%d/%m/%Y %H:%M:%S")
-    dataLog[timestamp] = {"hashedUid": hashedUid, "username": username,
-                          "action": action}
-    with open("log.json", "w") as f:
-        dump(dataLog, f)
+    with open("log.txt", "a") as f:
+        f.write(";".join((timestamp, hashedUid, username, action)))
