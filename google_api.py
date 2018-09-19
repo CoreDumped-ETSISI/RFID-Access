a06ce33871b632636c69c84af5377c57498d6469
Yadkee <yadkee@gmail.com>
2018-09-19 18:45:13 +0200

    Removed unused code and added time limit of half an hour

--- google_api.py
+++ google_api.py
@@ -3,9 +3,7 @@ import logging
 from oauth2client import file, client, tools
 from apiclient.discovery import build
 from httplib2 import Http
-from json import dump, load
-from sys import argv
-from os import remove
+from time import time
 
 """
 insert_user(hashedUid, status, name, email, phone, telegramName)
@@ -20,6 +18,7 @@ logger.setLevel(logging.DEBUG)
 USERS_SS_ID = "1tpHH2elJiTqIe3Dav4Hzjqab3Rs137OjOKd9W0oNxr8"
 # | Timestamp | hashedUid | username | action |
 LOG_SS_ID = "1ISaxhpRb2POD-7LvGGSdrpCnZQmTw-p17DOtXoRZKNg"
+LAST = [0]
 
 
 def _connect():
@@ -36,24 +35,9 @@ def _connect():
     return googleApi
 
 
-def insert_user(hashedUid, status, name, email, phone, telegramName):
-    googleApi = _connect()
-    try:
-        resource = {
-            "majorDimension": "ROWS",
-            "values": [[hashedUid, status, name, email, phone, telegramName]]}
-        googleApi.spreadsheets().values().append(
-            spreadsheetId=USERS_SS_ID,
-            range="usuarios!A1:F",
-            body=resource,
-            valueInputOption="RAW"
-        ).execute()
-        return True
-    except Exception as e:
-        logger.exception("Error al insertar usuario\n" + str(e))
-
-
 def get_users():
+    if LAST[-1] + 1800 > time():
+        raise Exception("Half an hour limit")
     googleApi = _connect()
     userData = {}
     result = googleApi.spreadsheets().values().get(
@@ -68,33 +52,5 @@ def get_users():
             "email": email,
             "phone": phone,
             "telegramUser": telegramUser}
-    with open("users.json", "w") as f:
-        dump(userData, f)
+    LAST.append(time())
     return userData
-
-
-def save_log():
-    googleApi = _connect()
-    try:
-        with open("log.json") as f:
-            logData = load(f)
-            data = [[timestamp, j["hashedUid"], j["username"], j["action"]]
-                    for timestamp, j in logData.items()]
-            resource = {
-                "majorDimension": "ROWS",
-                "values": data}
-            googleApi.spreadsheets().values().append(
-                spreadsheetId=LOG_SS_ID,
-                range="log!A:D",
-                body=resource,
-                valueInputOption="RAW"
-            ).execute()
-        remove("log.json")
-    except FileNotFoundError as e:
-        logger.exception("Ninguna actividad que guardar\n" + str(e))
-    except Exception as e:
-        logger.exception("Error al actualizar el log online\n" + str(e))
-
-
-if __name__ == "__main__" and "--update_log" in argv:
-    save_log()
